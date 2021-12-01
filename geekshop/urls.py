782e496e8f75f8a30f0437d656efe3be3f5efcb1
Onlyjoysprod <kujaku@yandex.ru>
2021-12-01 12:51:41 +0300

    Lesson5_HW_done

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
 
     path('products/', include('mainapp.urls', namespace='products')),
     path('auth/', include('authapp.urls', namespace='authapp')),
+    path('basket/', include('basketapp.urls', namespace='basketapp')),
 
     path('admin/', admin.site.urls),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,26 +1,48 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from mainapp.models import Product, ProductCategory
+from basketapp.models import Basket
 
 
 def index(request):
 
     products_list = Product.objects.all()[:4]
-    print(products_list.query)
 
     context = {
         'title': 'мой магазин',
         'products': products_list
     }
-    return render(request, 'mainapp/index.html', context=context)
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request, pk=None):
     links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products_list = Product.objects.all()
+            category_item = {'name': 'Все', 'pk': 0}
+        else:
+            category_item = get_object_or_404(ProductCategory, pk=pk)
+            products_list = Product.objects.filter(category__pk=pk)
+
+        context = {
+            'links_menu': links_menu,
+            'products': products_list,
+            'category': category_item,
+            'basket': Basket.objects.filter(user=request.user)
+        }
+
+        return render(request, 'mainapp/products_list.html', context)
+
+
     context = {
         'links_menu': links_menu,
-        'title': 'Товары'
+        'title': 'Товары',
+        'hot_product': Product.objects.all().first(),
+        'same_products': Product.objects.all()[3:5],
+        'basket': sum(list(Basket.objects.filter(user=request.user).values_list('quantity', flat=True)))
     }
     return render(request, 'mainapp/products.html', context)
 

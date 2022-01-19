27885cea7b73f3544e4275830dd0c00016be62cd
Onlyjoysprod <kujaku@yandex.ru>
2022-01-19 14:07:17 +0300

    some changes

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mainapp import views as mainapp
 
 urlpatterns = [
@@ -35,3 +35,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
